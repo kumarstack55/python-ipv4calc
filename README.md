5bbd66ccc09cbef9661ef8ef08dbaafd8ec22681
kumarstack55 <kumarstack55@gmail.com>
2022-02-14 21:55:10 +0900

    Add decimal input and output

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## Requirements
 
+* Python 3.3+
 * Poetry 1.1+
 
 ## Installation
@@ -15,17 +16,32 @@ poetry install
 ## Usage
 
 ```console
-PS > poetry run ipv4calc 192.168.0.1/24
-address         : 192.168.0.1
-network         : 192.168.0.0
-netmask         : 255.255.255.0
-broadcast       : 192.168.0.255
-addressPrefix   : 24
-numAddresses    : 256
-addressBinary   : 11000000.10101000.00000000.00000001
-networkBinary   : 11000000.10101000.00000000.00000000
-netmaskBinary   : 11111111.11111111.11111111.00000000
-broadcastBinary : 11000000.10101000.00000000.11111111
+PS > poetry run ipv4calc 192.168.1.0/24
+address          : 192.168.1.0
+network          : 192.168.1.0
+netmask          : 255.255.255.0
+broadcast        : 192.168.1.255
+addressPrefix    : 24
+numAddresses     : 256
+addressBinary    : 11000000.10101000.00000001.00000000
+networkBinary    : 11000000.10101000.00000001.00000000
+netmaskBinary    : 11111111.11111111.11111111.00000000
+broadcastBinary  : 11000000.10101000.00000001.11111111
+addressDecimal   : 3232235776
+networkDecimal   : 3232235776
+netmaskDecimal   : 4294967040
+broadcastDecimal : 3232236031
+```
+
+```console
+PS > poetry run ipv4calc -h
+usage: ipv4calc [-h] ipaddr_prefix
+
+positional arguments:
+  ipaddr_prefix  192.168.0.1/24 or 192.168.0.1 or 3232235521.
+
+optional arguments:
+  -h, --help     show this help message and exit
 ```
 
 ## License
--- src/ipv4calc/__init__.py
+++ src/ipv4calc/__init__.py
@@ -1,8 +1,9 @@
 from ipaddress import IPv4Interface
+from typing import Union
 import argparse
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 class KeyValuePrinter(object):
@@ -20,28 +21,55 @@ class KeyValuePrinter(object):
             print(format_string.format(key, value))
 
 
+def _to_dec(network):
+    value = 0
+    for index, octet in enumerate(network.packed):
+        value *= 2 ** 8
+        value += octet
+    return value
+
+
 def _to_bin(network):
     packed = network.packed
     return '.'.join(map(lambda o: '{:08b}'.format(o), map(int, packed)))
 
 
+def get_ipv4_address(address: str) -> Union[int, str]:
+    try:
+        return int(address)
+    except ValueError:
+        return address
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('ipaddr_prefix', nargs=1, help='x.x.x.x/xx')
+    parser.add_argument(
+            'ipaddr_prefix', nargs=1,
+            help='192.168.0.1/24 or 192.168.0.1 or 3232235521.')
     args = parser.parse_args()
-    interface = IPv4Interface(args.ipaddr_prefix[0])
+
+    interface = IPv4Interface(get_ipv4_address(args.ipaddr_prefix[0]))
+
+    address = interface.ip
+    network = interface.network.network_address
+    netmask = interface.network.netmask
+    broadcast = interface.network.broadcast_address
 
     p = KeyValuePrinter()
-    p.add('address', interface.ip)
-    p.add('network', interface.network.network_address)
-    p.add('netmask', interface.network.netmask)
-    p.add('broadcast', interface.network.broadcast_address)
+    p.add('address', address)
+    p.add('network', network)
+    p.add('netmask', netmask)
+    p.add('broadcast', broadcast)
     p.add('addressPrefix', interface.network.prefixlen)
     p.add('numAddresses', interface.network.num_addresses)
-    p.add('addressBinary', _to_bin(interface.ip))
-    p.add('networkBinary', _to_bin(interface.network.network_address))
-    p.add('netmaskBinary', _to_bin(interface.network.netmask))
-    p.add('broadcastBinary', _to_bin(interface.network.broadcast_address))
+    p.add('addressBinary', _to_bin(address))
+    p.add('networkBinary', _to_bin(network))
+    p.add('netmaskBinary', _to_bin(netmask))
+    p.add('broadcastBinary', _to_bin(broadcast))
+    p.add('addressDecimal', _to_dec(address))
+    p.add('networkDecimal', _to_dec(network))
+    p.add('netmaskDecimal', _to_dec(netmask))
+    p.add('broadcastDecimal', _to_dec(broadcast))
     p.print()
 
 
